6e68a85cbcb77e1370c534373ca07978cbd43382
melosh101 <git@milasholsting.dk>
2025-09-25 10:01:29 +0200

    prepare for auth

--- server/api/api.go
+++ server/api/api.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/melosh101/sweepMines/server/api/auth"
+)
+
+func Serve(router *gin.RouterGroup) {
+
+	router.GET("", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	authRouter := router.Group("auth")
+	auth.Serve(authRouter)
+}
--- server/api/auth/auth.go
+++ server/api/auth/auth.go
@@ -1,1 +1,19 @@
 package auth
+
+import "github.com/gin-gonic/gin"
+
+func Serve(router *gin.RouterGroup) {
+	router.POST("login", handleLogin)
+	router.POST("register", handleRegister)
+	router.GET("login", handleLogin)
+}
+
+func handleLogin(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func handleRegister(c *gin.Context) {
+
+}
